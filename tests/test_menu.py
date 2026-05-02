1a3739793e399e4456d5bfde0e20a619389426a1
Eva SV <evasauraklix@hotmail.com>
2026-05-02 12:42:09 +0200

    test menu 2

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,24 +1,32 @@
 from playwright.sync_api import Page, expect
 
-def test_navigation_through_links(page: Page):
+def test_navigation_through_about_link(page: Page):
+    url = 'https://web-qa.dev.adalab.es'
+    
     print("When the user enters the 'Vida Verde' page")
-    page.goto("https://web-qa.dev.adalab.es/")
+    page.goto(url)
     print("When the user clicks on 'Quienes Somos'")
     page.get_by_role("link", name="Quiénes Somos").click()
-    print ("And the user clicks on 'Products'")
-    page.get_by_role("link", name="Productos").click()
-    print ("And the user clicks on 'Contact'")
-    page.get_by_role("link", name="Contacto").click()
-
-    print("Then the 'Quienes somos' page is displayed")
-    page.get_by_role("link", name="Quiénes Somos").click()
     expect(page.get_by_role("heading", name="Quiénes Somos")).to_be_visible()
-    print("Then the 'Productos' page is displayed")
-    page.get_by_role("link", name="Productos").click()
-    expect(page.get_by_role("heading", name="Productos")).to_be_visible()
-    print("Then the 'Contáctanos' page is displayed")
-    page.get_by_role("link", name="Contáctanos").click()
-    expect(page.get_by_role("heading", name="Contáctanos")).to_be_visible()
+    expect(page).to_have_url(f'{url}/about')
 
-     
-    
+def test_navigation_through_products_links(page: Page):
+    url = 'https://web-qa.dev.adalab.es'
+
+    print("When the user enters the 'Vida Verde' page")
+    page.goto(url)
+    print("When the user clicks on 'Products'")
+    page.get_by_role("link", name="Productos", exact=True).click()
+    expect(page.get_by_role("heading", name="Productos")).to_be_visible()
+    expect(page).to_have_url(f'{url}/products')
+
+def test_navigation_through_contact_links(page: Page):
+    url = 'https://web-qa.dev.adalab.es'
+
+    print("When the user enters the 'Vida Verde' page")
+    page.goto(url)
+    print("When the user clicks on 'Contact Us'")
+    page.get_by_role("link", name="Contacto").click()
+    expect(page.get_by_role("heading", name="Contacto")).to_be_visible()
+    expect(page).to_have_url(f'{url}/contact')
+
