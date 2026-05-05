5cdbde075664867c63987a9022078c2d50ffe95a
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-05-05 12:12:05 +0100

    Update test_contacto.py

--- tests/test_contacto.py
+++ tests/test_contacto.py
@@ -16,7 +16,14 @@ def test_submit_form_with_required_message_field_left_empty(page: Page):
 from playwright.sync_api import Page, expect
 
 def test_Submit_form_with_required_name_field_empty(page: Page):
-
-    print("Given user visit homepage")
-    page.goto("https://web-qa.dev.adalab.es")
-    
+    print('Given the user enters the contact page "Contáctanos | Vida Verde"')
+    page.goto("https://web-qa.dev.adalab.es/contact")
+    print("When they fill in the required email field with 'test@gmail.com'")
+    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+    print("and fill in the required message field with 'test message'")
+    page.get_by_role("textbox", name="Mensaje *").fill("test mensaje")
+    print("And they click Submit")
+    page.get_by_role("button", name="Enviar Mensaje").click()
+    print('Then they should see an error message "El nombre es obligatorio"')
+    expect(page.get_by_text("El nombre es obligatorio")).to_be_visible()
+   
