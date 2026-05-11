a2d0dc91588471fbf1861d6197a3c13b679a6b5f
LuciaAlcarazQA <pqsi2017@gmail.com>
2026-05-11 16:06:22 +0100

    FILTROS

--- pages/products_page.py
+++ pages/products_page.py
@@ -32,3 +32,53 @@ class ProductsPage:
 
     def filter_by_palas(self):
         self.page.get_by_role("searchbox", name="Nombre").fill("palas")
+
+
+from playwright.sync_api import Page
+
+
+class ProductsPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.url = 'https://web-qa.dev.adalab.es/products'
+        self.title = "Catálogo de Productos"
+
+    def navigate(self):
+        self.page.goto("https://web-qa.dev.adalab.es/products")
+
+    def filter_by_name(self, name):
+        self.page.get_by_role("searchbox", name="Nombre").fill(name)
+
+    def filter_by_category(self, category):
+        self.page.get_by_label("Categoría").select_option(category)
+
+    def filter_by_min_price(self, min_price):
+        self.page.get_by_role(
+            "spinbutton",
+            name="Precio mínimo"
+        ).fill(min_price)
+
+    def filter_by_max_price(self, max_price):
+        self.page.get_by_role("spinbutton",name="Precio máximo").fill(max_price)
+
+
+from playwright.sync_api import Page
+class ProductsPage:
+    def __init__(self, page: Page):
+        self.page = page
+        self.url = 'https://web-qa.dev.adalab.es/products'
+        self.title = "Catálogo de Productos"
+
+
+class ProductsPage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def navigate(self):
+        self.page.goto("https://web-qa.dev.adalab.es/products")
+
+    def filter_by_name(self, name):
+        self.page.get_by_role("searchbox", name="Nombre").fill(name)
+
+    def get_no_results_message(self):
+        return self.page.get_by_text("No se encontraron productos")
--- tests/test_filtros.py
+++ tests/test_filtros.py
@@ -1,24 +1,41 @@
-
 from playwright.sync_api import Page, expect
+from pages.products_page import ProductsPage
+
 
 def test_filter_by_valid_name_price_and_category(page: Page):
-    print("Given the user opens the products page 'Nuestros Productos | Vida Verde'")
-    page.goto("https://web-qa.dev.adalab.es/products")
-    print("When the user filters by name 'Regadera'")
-    page.get_by_role("searchbox", name="Nombre").fill("regadera")
-    print("And filters by category 'Herramientas'")
-    page.get_by_label("CategoríaTodas las categorí").select_option("Herramientas")
-    print("And filters by minimum price '20'")
-    page.get_by_role("spinbutton", name="Precio mínimo").fill("20")
-    print("And filters by maximum price '25'")
-    page.get_by_role("spinbutton", name="Precio máximo").fill("25")
-    expect(page.get_by_text("No se encontraron productos")).to_be_visible()
+    products_page = ProductsPage(page)
 
-def test_filter_by_name_with_no_results(page: Page):
-    print("Given the user opens the products page 'Nuestros Productos | Vida Verde'")
-    page.goto("https://web-qa.dev.adalab.es/products")
-    print("When the user filters by name 'manzana'")
-    page.get_by_role("searchbox", name="Nombre").fill("manzana")
-    expect(page.get_by_text("No se encontraron productos")).to_be_visible()
+    print("Given la usuaria abre la página de productos")
+    products_page.navigate()
+
+    print("When filtra por nombre Regadera")
+    products_page.filter_by_name("Regadera")
+
+    print("And filtra por categoría Herramientas")
+    products_page.filter_by_category("Herramientas")
+
+    print("And filtra por precio mínimo 20")
+    products_page.filter_by_min_price("20")
+
+    print("And filtra por precio máximo 25")
+    products_page.filter_by_max_price("25")
+
+    print("Then debe ver Regadera Metálica")
+    expect(page.get_by_text("Regadera Metálica")).to_be_visible()
+
+
+
+
+def test_filter_without_results(page: Page):
+    products_page = ProductsPage(page)
+
+    print("Given la usuaria visita la página de productos")
+    products_page.navigate()
+
+    print("When filtra por nombre sin resultados")
+    products_page.filter_by_name("manzana")
+
+    print("Then debe ver mensaje de no productos")
+    expect(products_page.get_no_results_message()).to_be_visible()
 
 
