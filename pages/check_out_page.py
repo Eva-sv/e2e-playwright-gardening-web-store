09274cbc98a3dcab2f27a08f226f6f8eccbff82d
Eva SV <evasauraklix@hotmail.com>
2026-05-11 15:49:25 +0200

    refactorización checkout

--- pages/check_out_page.py
+++ pages/check_out_page.py
@@ -6,9 +6,53 @@ class CheckoutPage:
          self.url = 'https://web-qa.dev.adalab.es/checkout'
          self.title = "Finalizar Compra"
 
+    def verify_order_summary(self):
+        expect(self.page.get_by_role("heading", name="Resumen del Pedido")).to_be_visible()
+        expect(self.page.get_by_text("Juego de Palas15.99 €")).to_be_visible()
+        expect(self.page.get_by_text("Subtotal (1)15.99 €")).to_be_visible()
+        expect(self.page.get_by_text("IVA (21%)3.36 €")).to_be_visible()
+        expect(self.page.get_by_text("Envío5.00 €")).to_be_visible()
+        expect(self.page.get_by_text("Total24.35 €")).to_be_visible()
+
+    def verify_valid_name_field(self):
+      self.page.get_by_role("textbox", name="Nombre Completo *").fill("Maria Diaz")
+
+    def verify_valid_email_field(self):
+         self.page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+
+    def verify_valid_adress(self):
+         self.page.get_by_role("textbox", name="Dirección *").fill("CAlle Aragon, 25, Madrid")
+
+    def verify_valid_card_number(self):
+         self.page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("4242424242424242")
+
+    def verify_click_complete_purchase(self):
+        self.page.get_by_role("button", name="Completar Compra").click()
+
+    def verify_message_purchase_completed_succesfully(self):
+        expect(self.page.get_by_role("heading", name="¡Compra Realizada con Éxito!")).to_be_visible()
+
+    def verify_click_back_store(self):
+        self.page.get_by_role("link", name="Volver a la Tienda").click()
+
+    def verify_see_products_page(self):
+        self.expect(self.page.get_by_role("heading", name="Catálogo de Productos")).to_be_visible()
+
+    def verify_invalid_card_number(self):
+        self.page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").click()
+        self.page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("1111424242424242")
+    
+    def verify_click_complete_purchase(self):
+        self.page.get_by_role("button", name="Completar Compra").click()
+
+    def verify_message_invalid_card(self):
+        self.expect(self.page.get_by_text("Tarjeta de crédito no válida.")).to_be_visible()
+
+    def verify_remain_checkout_page_and_url_checkout(self):
+        self.expect(self.page.get_by_role("heading", name="Finalizar Compra")).to_be_visible()
 
     
-    
+
         
 
  
--- tests/test_check_out.py
+++ tests/test_check_out.py
@@ -3,14 +3,18 @@ from check_out_page import CheckoutPage
 from products_page import ProductsPage
 from shopping_cart_page import ShoppingcartPage
 
-url ='https://web-qa.dev.adalab.es/products'
 
 def test_checkout_with_valid_payment_details(page: Page):
+    products_page = ProductsPage(page)
+    check_out_page = CheckoutPage(page)
+    shopping_cart_page = ShoppingcartPage(page)
+
+
     
     # Logica del ProductsPAge
     print("When the user enters to the products page")
     ProductsPage.open_products_page()
-    
+
     print("When they filter by name 'palas'")
     page.get_by_role("searchbox", name="Nombre").fill("palas")
     print("And add the product to the cart")
@@ -22,90 +26,102 @@ def test_checkout_with_valid_payment_details(page: Page):
     print("And click on 'Proceed to checkout'")
     page.get_by_role("link", name="Proceder al Pago").click()
     
-    # Logica CheckoutPage
     print("Then they should see the order summary with:'product:juego de palas','price: 15.99', 'subtotal:15.99','iva: 3.36', 'shipping: 5', 'total: 24'")
-    expect(page.get_by_role("heading", name="Resumen del Pedido")).to_be_visible()
-    expect(page.get_by_role("heading", name="Resumen del Pedido")).to_be_visible()
-    expect(page.get_by_text("Juego de Palas15.99 €")).to_be_visible()
-    expect(page.get_by_text("Subtotal (1)15.99 €")).to_be_visible()
-    expect(page.get_by_text("IVA (21%)3.36 €")).to_be_visible()
-    expect(page.get_by_text("Envío5.00 €")).to_be_visible()
-    expect(page.get_by_text("Total24.35 €")).to_be_visible()
-
+    #ShoppingCartPage.verify_order_summary()
 
     print("When they fill in the valid name field 'Maria Diaz'")
-    page.get_by_role("textbox", name="Nombre Completo *").fill("Maria Diaz")
+    CheckoutPage.verify_valid_name_field()
+
     print("And fill in the valid email field")
-    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+    CheckoutPage.verify_valid_email_field()
+
     print("And fill in the valid adress 'Calle Aragón, 25, Madrid'")
-    page.get_by_role("textbox", name="Dirección *").fill("CAlle Aragon, 25, Madrid")
+    CheckoutPage.verify_valid_adress()
+
     print("And add a valid card number '4242 4242 4242 4242'")
-    page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("4242424242424242")
+    CheckoutPage.verify_valid_card_number()
+
     print("And click on 'Complete purchase'")
-    page.get_by_role("button", name="Completar Compra").click()
+    CheckoutPage.verify_click_complete_purchase()
+
     print("Then they should see the message 'Purchase completed successfully")
-    expect(page.get_by_role("heading", name="¡Compra Realizada con Éxito!")).to_be_visible()
+    CheckoutPage.verify_message_purchase_completed_succesfully()
+
     print("And they should see the completed order page with: 'Resumen del pedido")
-    expect(page.get_by_role("heading", name="¡Compra Realizada con Éxito!")).to_be_visible()
-    expect(page.get_by_role("heading", name="Resumen del Pedido")).to_be_visible()
-    expect(page.get_by_text("Juego de Palas15.99 €")).to_be_visible()
-    expect(page.get_by_text("Subtotal (1)15.99 €")).to_be_visible()
-    expect(page.get_by_text("IVA (21%)3.36 €")).to_be_visible()
-    expect(page.get_by_text("Envío5.00 €")).to_be_visible()
-    expect(page.get_by_text("Total22.35 €")).to_be_visible()
+    CheckoutPage.verify_order_summary()
 
     print("When they click on 'Back to store'")
-    page.get_by_role("link", name="Volver a la Tienda").click()
+    CheckoutPage.verify_click_back_store()
+
     print("Then they should see the products page: https://web_qa.dev.adalab.es/products")
-    expect(page.get_by_role("heading", name="Catálogo de Productos")).to_be_visible()
+    CheckoutPage.verify_see_products_page()
 
     
 def test_checkout_with_invalid_card_details(page: Page):
     print("When the user enters to the products page")
-    page.goto(url)
+    ProductsPage.open_products_page()
+
     print("When they filter by name 'palas'")
     page.get_by_role("searchbox", name="Nombre").fill("palas")
+
     print("And add the product to the cart")
     page.get_by_role("button", name="Añadir Juego de Palas al").click()
+
     print("And visit the cart page")
     page.get_by_role("link", name="Carrito de compra").click()
+
     print("And click on 'Proceed to checkout'")
     page.get_by_role("link", name="Proceder al Pago").click()
+
     print("When they fill in the valid name field 'Maria Diaz'")
-    page.get_by_role("textbox", name="Nombre Completo *").fill("Maria Diaz")
+    CheckoutPage.verify_valid_name_field()
+
     print("And fill in the valid email field")
-    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+    CheckoutPage.verify_valid_email_field()
+
     print("And fill in the valid adress 'Calle Aragón, 25, Madrid'")
-    page.get_by_role("textbox", name="Dirección *").fill("CAlle Aragon, 25, Madrid")
+    CheckoutPage.verify_valid_adress()
+
     print("And add an invalid card number '1111 4242 4242 4242'")
-    page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").click()
-    page.get_by_role("textbox", name="Número de Tarjeta de Crédito *").fill("1111424242424242")
+    CheckoutPage.verify_invalid_card_number()
+
     print("And click on 'Complete purchase'")
-    page.get_by_role("button", name="Completar Compra").click()
+    CheckoutPage.verify_click_complete_purchase()
+
     print("Then they should see a message with 'Tarjeta de crédito no válida'")
-    expect(page.get_by_text("Tarjeta de crédito no válida.")).to_be_visible()
+    CheckoutPage.verify_invalid_card_number()
 
 
 def test_checkout_without_card_details(page: Page):
+
     print("When the user enters to the products page")
-    page.goto(url)
+    ProductsPage.open_products_page()
+
     print("When they filter by name 'palas'")
     page.get_by_role("searchbox", name="Nombre").fill("palas")
+
     print("And add the product to the cart")
     page.get_by_role("button", name="Añadir Juego de Palas al").click()
+
     print("And visit the cart page")
     page.get_by_role("link", name="Carrito de compra").click()
+
     print("And click on 'Proceed to checkout'")
     page.get_by_role("link", name="Proceder al Pago").click()
+
     print("When they fill in the valid name field 'Maria Diaz'")
-    page.get_by_role("textbox", name="Nombre Completo *").fill("Maria Diaz")
+    CheckoutPage.verify_valid_name_field()
+
     print("And fill in the valid email field")
-    page.get_by_role("textbox", name="Email *").fill("test@gmail.com")
+    CheckoutPage.verify_valid_email_field()
+
     print("And fill in the valid adress 'Calle Aragón, 25, Madrid'")
-    page.get_by_role("textbox", name="Dirección *").fill("CAlle Aragon, 25, Madrid")
+    CheckoutPage.verify_valid_adress()
+
     print("And click on 'Complete purchase'")
-    page.get_by_role("button", name="Completar Compra").click()
+    CheckoutPage.verify_click_complete_purchase()
+    
     print("Then the user should remain on the checkout page and the page URL should be 'https://web-qa.dev.adalab.es/checkout'")
-    expect(page.get_by_role("heading", name="Finalizar Compra")).to_be_visible()
+    CheckoutPage.verify_remain_checkout_page_and_url_checkout()
 
     
