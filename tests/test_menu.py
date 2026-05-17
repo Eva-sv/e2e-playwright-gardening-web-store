23f464e243cb10773041094109d00f75db25ae4e
Yohana Triana <ytaranzola@gmail.com>
2026-05-17 15:41:22 +0200

    Summary

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,9 +1,10 @@
 from playwright.sync_api import Page, expect
+from pages import contact_page
 from pages.components.menu import Menu
 from pages.home_page import HomePage
 from pages.about_us_page import AboutusPage
 from pages.products_page import ProductsPage
-from pages.contacto_page import ContactPage
+from pages.contact_page import ContactPage
 
 
 
@@ -12,26 +13,26 @@ def test_visit_menu_links(page: Page):
     menu = Menu(page)
     about_us_page = AboutusPage(page)
     products_page = ProductsPage(page)
-    contacto_page = ContactPage(page)
-   
+    contacto_page = ContactPage(page)  
 
 
-    print("When the user enters the 'Vida Verde' page")
+    print("Given the user enters the 'Vida Verde' page")
     home_page.open_home_page()
 
     print("Then they should see the title 'Vida Verde'")
     home_page.verify_home_page_title()
 
-    print("When the user clicks on 'Quienes Somos'")
+    print("When the user clicks on 'Quiénes Somos'")
     menu.visit_menu_about_us()
 
-    print("Then they should see the tittle 'Quienes Somos'")
+    print("Then they should see the tittle 'Quiénes Somos'")
     about_us_page.verify_about_us_title()
 
     print("And they should see the URL 'https://web-qa.dev.adalab.es/about'")
     about_us_page.verify_about_us_url()
     
     print("When the user clicks on 'Productos'")
+    page.get_by_role("button", name="Abrir menú principal").click()
     menu.visit_menu_products()
 
     print("Then they should see the tittle 'Catálogo de Productos'")
@@ -41,13 +42,16 @@ def test_visit_menu_links(page: Page):
     products_page.verify_products_url()
 
     print("When the user clicks on 'Contacto'")
-    menu.visit_menu_contacto()
+    page.get_by_role("button", name="Abrir menú principal").click()
+    menu.visit_menu_contact()
 
     print("Then they should see the tittle 'Contáctanos'")
-    contacto_page.verify_contact_title()
+    contact_page.verify_contact_title()
+    #contact_page.verify_contact_title()
 
     print("And they should see the URL 'https://web-qa.dev.adalab.es/contact'")
-    contacto_page.verify_contact_url()
+    contact_page.verify_contact_url()
+
 
 
 
